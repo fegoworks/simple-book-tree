5d1cae5f8c306feec0fdb736aceb035b1628cc8f
Edafe Oghenefego <47613811+fegoworks@users.noreply.github.com>
2021-11-20 09:30:26 +0100

    initial commit

--- src/components/Courses.jsx
+++ src/components/Courses.jsx
@@ -1,22 +1,22 @@
-import React, { useState, useEffect } from "react";
-import { courses as courseData } from "../data/courses";
+import React, { useState } from "react";
+import { courses } from "../data/courses";
 import Course from "./Course";
 import "../styling/_course.scss";
 
 const Courses = () => {
-  const [courses, setCourses] = useState([]);
   const [active, setActive] = useState();
-
-  useEffect(() => {
-    if (courseData) {
-      setCourses(courseData);
-    }
-  }, [courseData]);
 
   return (
     <div className="courses">
-      {courses.map((course) => {
-        return <Course course={course} active={active} setActive={setActive} />;
+      {courses.map((course, index) => {
+        return (
+          <Course
+            key={index}
+            course={course}
+            active={active}
+            setActive={setActive}
+          />
+        );
       })}
     </div>
   );
